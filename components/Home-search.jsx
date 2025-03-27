5d9b78086529198a27bc60b1632c9223babf384a
Sandeep Singh <snayal50@gmail.com>
2025-03-27 10:06:17 +0530

    home page done

--- components/Home-search.jsx
+++ components/Home-search.jsx
@@ -1,8 +1,29 @@
+"use client"
+
+import { useState } from "react"
+import { Input } from "./ui/input"
 
 
 const HomeSearch = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleTextSubmit = () => { }
+
     return (
-        <div>Home-search</div>
+        <div>
+            <form onSubmit={handleTextSubmit}>
+                <div className="relative flex items-center">
+
+                    <Input
+                        type="text"
+                        placeholder="Enter make,model or use our AI Image Search"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="pl-10 pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-sm"
+                    />
+                </div>
+            </form>
+        </div>
     )
 }
 
